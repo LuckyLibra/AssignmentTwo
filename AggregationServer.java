01168e841f812647c564ba69ce738188093e195b
LuckyLibra <annie-salt@outlook.com>
2023-09-04 12:35:50 +0930

    also added example file provided

--- AggregationServer.java
+++ AggregationServer.java
@@ -1,5 +1,12 @@
 package AssignmentTwo;
 
 public class AggregationServer {
+    private int port = 4567; //default port
+    private int lamport_clock = 0; //default value for lamport clock
+
+    
+
+
+
     
 }
