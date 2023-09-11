d975cf39c531b4e0ced4baf0a79dfc9be62b7861
LuckyLibra <annie-salt@outlook.com>
2023-09-11 18:53:31 +0930

    created a read file into json function on the content server and began to fill out the aggregation server and content server with socket connections

--- AggregationServer.java
+++ AggregationServer.java
@@ -1,12 +1,31 @@
 package AssignmentTwo;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class AggregationServer {
-    private int port = 4567; //default port
-    private int lamport_clock = 0; //default value for lamport clock
+    public static void main(String [] args){
+        int port = 4567; //default number
+        int lamport_clock = 0; //lamport clock
 
-    
+        if ( args.length > 0 ){ //If a port number was in the command line
+            port = Integer.parseInt(args[0]); //Update port number to command line arg
+        }
+
+        try {
+            ServerSocket serverSocket = new serverSocket(port); //open socket
+
+            while (true) {
+                Socket.clientSocket = serverSocket.accept(); // wait for connections
+
+                
+
+            }
+        }
 
 
 
-    
+    }
 }
